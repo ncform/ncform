7989eb871ed7c32e9fa7a0494308920b28a47120
daniel-dx <danieldx666@126.com>
2019-03-10 16:08:44 +0800

    test(ncform): optimize the ui.description test case

--- packages/ncform-e2e/ncform/cypress/integration/examples/ui.description.spec.js
+++ packages/ncform-e2e/ncform/cypress/integration/examples/ui.description.spec.js
@@ -13,12 +13,21 @@ context('ui.description', () => {
     cy.get(`[data-cy=${id}]`).within(() => {
       cy.contains('text value desc').should('be.visible');
       cy.contains('html value desc').should('be.visible');
-      cy.contains('html value desc').then(($desc) => {
-        cy.wrap($desc.css('color')).should('equal', 'rgb(103, 194, 58)');
-      })
+      cy.contains('html value desc').should('have.css', 'color', 'rgb(103, 194, 58)');
       cy.contains('dx value: daniel').should('not.exist');
       cy.get('label').contains('name1').next().find('input').type('daniel');
       cy.contains('dx value: daniel').should('exist');
+    })
+  })
+
+  it('Array-table: Show description dx', () => {
+    let id = md5('Array-table: Show description dx');
+    cy.get(`[data-cy=${id}]`).within(() => {
+      cy.get('th').contains('users').as('header');
+      cy.get('@header').find('small').should('have.text', 'dx desc: ');
+
+      cy.get('label').contains('company').next().find('input').type('google');
+      cy.get('@header').find('small').should('have.text', 'dx desc: google');
     })
   })
 
