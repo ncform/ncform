d0016f69cbd54aae15120cd59d25aafb211e0869
daniel.xiao <danieldx666@126.com>
2019-03-11 17:09:37 +0800

    test: optimize then nesting test cases

--- packages/ncform-e2e/ncform/cypress/integration/examples/nesting.spec.js
+++ packages/ncform-e2e/ncform/cypress/integration/examples/nesting.spec.js
@@ -14,6 +14,9 @@ context('Nesting', () => {
       cy.get('legend').contains('user').as('user').should('exist');
       cy.get('@user').next().find('legend').contains('name').as('name').should('exist');
       cy.get('@name').next().find('label:contains("name")').its('length').should('equal', 2);
+
+      cy.get('@name').next().find('input').eq(0).should('have.value', 'daniel')
+      cy.get('@name').next().find('input').eq(1).should('have.value', 'xiao')
     })
   })
 
@@ -25,10 +28,12 @@ context('Nesting', () => {
       cy.get('@company').next().find('legend').contains('users1').as('users1').should('exist');
       cy.get('@users1').next().find('label:contains("users1 1")').its('length').should('equal', 1);
       cy.get('@users1').next().find('input').its('length').should('equal', 1);
+      cy.get('@users1').next().find('input').should('have.value', 'daniel');
 
       cy.get('@company').next().find('legend').contains('users2').as('users2').should('exist');
       cy.get('@users2').next().find('th:contains("users2")').should('exist');
       cy.get('@users2').next().find('input').its('length').should('equal', 1);
+      cy.get('@users2').next().find('input').should('have.value', 'sarah');
     })
   })
 
@@ -39,10 +44,12 @@ context('Nesting', () => {
       cy.get('@companies1').next().find('label:contains("companies1 1")').as('companies1Item').should('exist');
       cy.get('@companies1Item').next().next().find('label:contains("name")').its('length').should('equal', 1);
       cy.get('@companies1Item').next().next().find('input').its('length').should('equal', 1);
+      cy.get('@companies1Item').next().next().find('input').should('have.value', 'daniel');
 
       cy.get('legend').contains('companies2').as('companies2').should('exist');
       cy.get('@companies2').next().find('th:contains("name")').should('exist');
       cy.get('@companies2').next().find('input').its('length').should('equal', 1);
+      cy.get('@companies2').next().find('input').should('have.value', 'sarah');
     })
   })
 
@@ -54,24 +61,28 @@ context('Nesting', () => {
       cy.get('@companies1Item').next().next().find('legend').contains('User').as('users').should('exist');
       cy.get('@users').next().find('label').contains('User 1').should('exist');
       cy.get('@users').next().find('input').its('length').should('equal', 1);
+      cy.get('@users').next().find('input').should('have.value', 'daniel');
 
       cy.get('legend').contains('companies2').as('companies2').should('exist');
       cy.get('@companies2').next().find('label:contains("companies2 1")').as('companies2Item').its('length').should('equal', 1);
       cy.get('@companies2Item').next().next().find('legend').contains('User').as('users').should('exist');
       cy.get('@users').next().find('th').contains('User').should('exist');
       cy.get('@users').next().find('input').its('length').should('equal', 1);
+      cy.get('@users').next().find('input').should('have.value', 'sarah');
 
       cy.get('legend').contains('companies3').as('companies3').should('exist');
       cy.get('@companies3').next().find('th').contains('companies3').should('exist');
       cy.get('@companies3').next().find('td').find('legend').contains('User').as('users').should('exist');
       cy.get('@users').next().find('label:contains("User 1")').should('exist');
       cy.get('@users').next().find('input').its('length').should('equal', 1);
+      cy.get('@users').next().find('input').should('have.value', 'daniel');
 
       cy.get('legend').contains('companies4').as('companies4').should('exist');
       cy.get('@companies4').next().find('th').contains('companies4').should('exist');
       cy.get('@companies4').next().find('td').find('legend').contains('User').as('users').should('exist');
       cy.get('@users').next().find('th').contains('User').should('exist');
       cy.get('@users').next().find('input').its('length').should('equal', 1);
+      cy.get('@users').next().find('input').should('have.value', 'sarah');
     })
   })
 
