e21f712db3b6dcc8fc1978976ecdfc45b0a8fec5
F-loat <chaimaoyuan@foxmail.com>
2019-11-20 11:38:40 +0800

    chore: adjust code indent

--- packages/ncform-theme-elementui/src/components/control-comps/checkbox.vue
+++ packages/ncform-theme-elementui/src/components/control-comps/checkbox.vue
@@ -1,44 +1,44 @@
 <template>
-  <div
-    class="ncform-checkbox"
-    v-show="!hidden"
-  >
-    <div v-if="globalStatus === 'preview'" class="ncform-checkbox-preview">
-      {{modelVal}}
-    </div>
-    <template v-else>
-      <el-checkbox
-        v-if="mergeConfig.selectAll && !readonly"
-        :class="'check-all'"
-        :disabled="disabled"
-        :indeterminate="isIndeterminate"
-        v-model="isCheckAll"
-        @change="handleCheckAllChange"
-      >{{$nclang('all')}}</el-checkbox>
-
-      <el-checkbox-group
-        v-if="!readonly"
-        size="mini"
-        :disabled="disabled"
-        v-model="modelVal"
-        @change="handleCheckedOptChange"
-      >
-        <component :is="'el-checkbox' + (mergeConfig.type === 'button' ? '-button' : '')"
-          v-for="opt in dataSource"
-          :key="opt[mergeConfig.itemValueField]"
-          :label="opt[mergeConfig.itemValueField]"
-          :class="mergeConfig.type === 'checkbox' && mergeConfig.arrangement === 'v' ? 'is-vertical' : ''"
-        >{{opt[mergeConfig.itemLabelField]}}</component>
-      </el-checkbox-group>
-
-      <label
-        v-show="readonly"
-        v-for="(label, idx) in labelRead"
-        :key="idx"
-        :class="['label-read', mergeConfig.type === 'checkbox' && mergeConfig.arrangement === 'v' ? 'label-vertical' : '']"
-      >{{label}}</label>
-    </template>
+<div
+  class="ncform-checkbox"
+  v-show="!hidden"
+>
+  <div v-if="globalStatus === 'preview'" class="ncform-checkbox-preview">
+    {{modelVal}}
   </div>
+  <template v-else>
+    <el-checkbox
+      v-if="mergeConfig.selectAll && !readonly"
+      :class="'check-all'"
+      :disabled="disabled"
+      :indeterminate="isIndeterminate"
+      v-model="isCheckAll"
+      @change="handleCheckAllChange"
+    >{{$nclang('all')}}</el-checkbox>
+
+    <el-checkbox-group
+      v-if="!readonly"
+      size="mini"
+      :disabled="disabled"
+      v-model="modelVal"
+      @change="handleCheckedOptChange"
+    >
+      <component :is="'el-checkbox' + (mergeConfig.type === 'button' ? '-button' : '')"
+        v-for="opt in dataSource"
+        :key="opt[mergeConfig.itemValueField]"
+        :label="opt[mergeConfig.itemValueField]"
+        :class="mergeConfig.type === 'checkbox' && mergeConfig.arrangement === 'v' ? 'is-vertical' : ''"
+      >{{opt[mergeConfig.itemLabelField]}}</component>
+    </el-checkbox-group>
+
+    <label
+      v-show="readonly"
+      v-for="(label, idx) in labelRead"
+      :key="idx"
+      :class="['label-read', mergeConfig.type === 'checkbox' && mergeConfig.arrangement === 'v' ? 'label-vertical' : '']"
+    >{{label}}</label>
+  </template>
+</div>
 </template>
 
 <style lang="scss">
